7eb7950c60557a7ecb44d77eae9deff585be4135
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-02-03 15:46:16 -0800

    update: probably works better now, can't test because i'm on linux

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import pyautogui
+import keyboard
 from time import sleep
-mousepos=[]
-sleep(3)
-x1=pyautogui.position()[0]
-y1=pyautogui.position()[1]
-print(x1, y1) 
-sleep(3)
-x2=pyautogui.position()[0]
-y2=pyautogui.position()[1]
-print(x2, y2)
-new1 = x2-x1
-new2 = y2-y1
-for x in [x1, y1, new1, new2]:
-  mousepos.append(x)
-print(mousepos) 
-pyautogui.screenshot('test.png', region=mousepos)
+import os
+
+
+os.environ['XAUTHORITY'] = '/home/kaleb/.Xauthority'
+
+os.system('xauth generate :0 . trusted')
+
+    
+    
+    
+class regionfinder:
+  x1 = None
+  x2 = None
+  def pos(self):
+    position=pyautogui.position()
+    if self.x1 == None:
+      self.x1 = position[0]
+      self.y1 = position[1]
+      print(f'''pos1: x: {self.x1}  y: {self.y1}''')
+    
+    else:
+      self.x2 = position[0]
+      self.y2 = position[1]
+      print(f'''pos2: x: {self.x2}  y: {self.y2}''')
+      self.x2 = self.x2-self.x1
+      self.y2 = self.y2-self.y1
+
+  
+  def screenshot(self):
+    mousepos = [self.x1, self.y1, self.x2, self.y2]
+    pyautogui.screenshot('test.png', region=mousepos)#type: ignore
+
+
+rf = regionfinder()
+
+while True: 
+  if keyboard.is_pressed('i'):
+    rf.pos()
+    sleep(2)
